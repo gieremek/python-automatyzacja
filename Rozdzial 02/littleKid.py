bb253b45c6c454e7d82d5e9a19cef465a7775d4a
gieremek <30390778+gieremek@users.noreply.github.com>
2018-12-04 23:06:57 +0100

    Create littleKid.py

---
+++ Rozdzial 02/littleKid.py
@@ -0,0 +1,16 @@
+# Rozdział drugi - sterowanie przepływem
+#********** Pobieranie danych *************************
+# Prośba o podanie imienia
+print('Jak masz na imie?') 
+name = input()
+# Prośba o podanie wieku
+print('Ile masz lat')
+age = int(input())  #konwersja na liczbe calkowita
+
+#********* Logika aplikacji **************************
+if name == 'Alicja' :
+    print('Witaj, Alicja.')
+elif age < 12 :
+    print('Nie jesteś Alicją, dzieciaku.')
+else :
+    print('Nie jesteś ani Alicją ani dzieciakiem.')
