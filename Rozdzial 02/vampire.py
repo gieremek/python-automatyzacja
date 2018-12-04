215727129837e98b912aeb51aaca3ee9805148a9
gieremek <30390778+gieremek@users.noreply.github.com>
2018-12-04 22:54:25 +0100

    Create vampire.py

---
+++ Rozdzial 02/vampire.py
@@ -0,0 +1,15 @@
+# Rozdział drugi - sterowanie przepływem
+#********** Pobieranie danych *************************
+# Prośba o podanie imienia
+print('Jak masz na imie?') 
+name = input()
+# Prośba o podanie wieku
+print('Ile masz lat')
+age = int(input())  #konwersja na liczbe calkowita
+
+#********* Logika aplikacji **************************
+if name == 'Alicja' :
+    print('Witaj, Alicja.')
+elif age < 12 :
+    print('Nie jesteś Alicją, dzieciaku.')
+elif age
